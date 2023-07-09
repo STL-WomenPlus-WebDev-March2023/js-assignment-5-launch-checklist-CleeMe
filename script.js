cd9af0100abe31b66cf402c7b6912e01b64bb92f
Christi-Lee <133257586+CleeMe@users.noreply.github.com>
2023-07-09 16:16:22 -0500

    all the changes from the extra long TA session.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function(){
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event){
+        let pilotName = document.querySelector("input[name=pilotName]");
+        let copilotName = document.querySelector("input[name=copilotName]"); 
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoMass = document.querySelector("input[name=cargoMass]");
+      
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+        alert("All fields are required!");
+        event.preventDefault();
+      }
+    });
+
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
@@ -14,3 +27,6 @@ window.addEventListener("load", function() {
    })
    
 });
+
+
+
